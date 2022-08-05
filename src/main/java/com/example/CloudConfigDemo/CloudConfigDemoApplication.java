66bc2e32b51c84ce6efd902a6fdc4e09a6eeb49b
illumis-git <51729245+illumis-git@users.noreply.github.com>
2022-08-05 22:05:22 +0900

    220805

---
+++ src/main/java/com/example/CloudConfigDemo/CloudConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudConfigDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CloudConfigDemo/CloudConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudConfigDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
